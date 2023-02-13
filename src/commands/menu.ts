4c158e88076e6803fa98b11766c45d267fdb82ab
Beast666 <khoapronk@gmail.com>
2023-02-13 17:46:09 +0700

    update packages

--- src/commands/menu.ts
+++ src/commands/menu.ts
@@ -1,21 +1,17 @@
-import type {
-  CommandInteraction,
-  MessageActionRowComponentBuilder,
-  SelectMenuInteraction,
-} from "discord.js";
-import { ActionRowBuilder, SelectMenuBuilder } from "discord.js";
-import { Discord, SelectMenuComponent, Slash } from "discordx";
+import type { CommandInteraction, MessageActionRowComponentBuilder, StringSelectMenuInteraction } from 'discord.js';
+import { ActionRowBuilder, StringSelectMenuBuilder } from 'discord.js';
+import { Discord, SelectMenuComponent, Slash } from 'discordx';
 
 const roles = [
-  { label: "Principal", value: "principal" },
-  { label: "Teacher", value: "teacher" },
-  { label: "Student", value: "student" },
+  { label: 'Principal', value: 'principal' },
+  { label: 'Teacher', value: 'teacher' },
+  { label: 'Student', value: 'student' },
 ];
 
 @Discord()
 export class Example {
-  @SelectMenuComponent({ id: "role-menu" })
-  async handle(interaction: SelectMenuInteraction): Promise<unknown> {
+  @SelectMenuComponent({ id: 'role-menu' })
+  async handle(interaction: StringSelectMenuInteraction): Promise<unknown> {
     await interaction.deferReply();
 
     // extract selected value by member
@@ -23,36 +19,29 @@ export class Example {
 
     // if value not found
     if (!roleValue) {
-      return interaction.followUp("invalid role id, select again");
+      return interaction.followUp('invalid role id, select again');
     }
 
     await interaction.followUp(
-      `you have selected role: ${
-        roles.find((r) => r.value === roleValue)?.label ?? "unknown"
-      }`
+      `you have selected role: ${roles.find((r) => r.value === roleValue)?.label ?? 'unknown'}`,
     );
     return;
   }
 
-  @Slash({ description: "roles menu", name: "my_roles" })
+  @Slash({ description: 'roles menu', name: 'my_roles' })
   async myRoles(interaction: CommandInteraction): Promise<unknown> {
     await interaction.deferReply();
 
     // create menu for roles
-    const menu = new SelectMenuBuilder()
-      .addOptions(roles)
-      .setCustomId("role-menu");
+    const menu = new StringSelectMenuBuilder().addOptions(roles).setCustomId('role-menu');
 
     // create a row for message actions
-    const buttonRow =
-      new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-        menu
-      );
+    const buttonRow = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(menu);
 
     // send it
     interaction.editReply({
       components: [buttonRow],
-      content: "select your role!",
+      content: 'select your role!',
     });
     return;
   }

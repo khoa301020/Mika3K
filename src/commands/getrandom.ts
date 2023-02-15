037ebed7a0ec4f7fcc50d474df48f713ae29da6f
Beast666 <khoapronk@gmail.com>
2023-02-15 17:37:41 +0700

    Feature: Random nuke

--- src/commands/getrandom.ts
+++ src/commands/getrandom.ts
@@ -1,15 +1,6 @@
 import axios from 'axios';
-import {
-  ActionRowBuilder,
-  ApplicationCommandOptionType,
-  ButtonBuilder,
-  ButtonInteraction,
-  ButtonStyle,
-  CommandInteraction,
-  EmbedBuilder,
-  MessageActionRowComponentBuilder,
-} from 'discord.js';
-import { ArgsOf, ButtonComponent, Discord, On, Slash, SlashOption } from 'discordx';
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import { Discord, SimpleCommand, SimpleCommandMessage, Slash } from 'discordx';
 import { Constants } from '../constants/constants.js';
 
 let config = {
@@ -19,23 +10,12 @@ let config = {
   },
 };
 
-const regexNum = /^\d+$/;
-
 @Discord()
 class Example {
-  @Slash({ description: 'Check NHentai nuke code', name: 'check-nuke' })
-  async checkCode(
-    @SlashOption({
-      description: 'NHentai code',
-      name: 'code',
-      required: true,
-      type: ApplicationCommandOptionType.Integer,
-    })
-    code: String,
-    interaction: CommandInteraction,
-  ): Promise<void> {
+  @Slash({ description: 'Get random NHentai nuke code', name: 'random-nuke' })
+  async getRandomSlash(interaction: CommandInteraction): Promise<void> {
     axios
-      .get(`${Constants.NHENTAI_API}/get?book=${code}`, config)
+      .get(`${Constants.NHENTAI_API}/random`, config)
       .then((res) => {
         const book = res.data.data;
         const embed = new EmbedBuilder()
@@ -70,26 +50,10 @@ class Example {
       });
   }
 
-  @On({ event: 'messageCreate' })
-  async onMessage([message]: ArgsOf<'messageCreate'>) {
-    if (message.author.bot || !regexNum.test(message.content)) return false;
-    if (parseInt(message.content) < 0 || parseInt(message.content) > 999999) return false;
-    const confirmBtn = new ButtonBuilder().setLabel('	|_・)').setStyle(ButtonStyle.Primary).setCustomId('get-nuke');
-
-    const row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(confirmBtn);
-
-    message.reply({ content: 'Nuke?', components: [row] }).then((msg) => {
-      setTimeout(() => msg.edit({ content: 'Nuke?', components: [] }), 60 * 1000);
-    });
-  }
-
-  @ButtonComponent({ id: 'get-nuke' })
-  async confirmBtn(interaction: ButtonInteraction): Promise<void> {
-    const codeMessageId = interaction.message.reference?.messageId!;
-    const message = await interaction.channel?.messages.fetch(codeMessageId)!;
-
+  @SimpleCommand({ aliases: ['nhrandom', 'nr'], description: 'Get random NHentai nuke code' })
+  getRandomCommand(command: SimpleCommandMessage): void {
     axios
-      .get(`${Constants.NHENTAI_API}/get?book=${message.content}`, config)
+      .get(`${Constants.NHENTAI_API}/random`, config)
       .then((res) => {
         const book = res.data.data;
         const embed = new EmbedBuilder()
@@ -97,8 +61,8 @@ class Example {
           .setTitle(book.title)
           .setURL(res.data.source)
           .setAuthor({
-            name: `${message.author.username}#${message.author.discriminator}`,
-            iconURL: message.author.displayAvatarURL(),
+            name: `${command.message.author.username}#${command.message.author.discriminator}`,
+            iconURL: command.message.author.displayAvatarURL(),
           })
           .setDescription(book.optional_title ? book.optional_title.english : 'No optional names.')
           .setThumbnail(Constants.NHENTAI_LOGO)
@@ -116,11 +80,11 @@ class Example {
           .addFields({ name: 'Upload date', value: book.upload_date })
           .setFooter({ text: 'NHentai', iconURL: Constants.NHENTAI_LOGO });
 
-        interaction.reply({ embeds: [embed], ephemeral: true });
+        command.message.reply({ embeds: [embed] });
       })
       .catch((err) => {
         console.log(err);
-        interaction.reply({ content: err.message, ephemeral: true });
+        command.message.reply({ content: err.message });
       });
   }
 }
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,6 @@
 export class Constants {
   public static readonly NHENTAI_API = 'https://janda.mod.land/nhentai';
   public static readonly NHENTAI_LOGO = 'https://archive.org/download/nhentai-logo-3/nhentai-logo-3.jpg';
+  public static readonly USER_AGENT =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
 }
